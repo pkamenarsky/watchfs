931e4d43e8fd1e3b775561f8925a253d27cb7809
Philip Kamenarsky <phil@Philips-MacBook-Pro.local>
2013-01-07 15:48:23 +0100

    , ,

--- watchr.js
+++ watchr.js
@@ -1,8 +1,11 @@
+#!/usr/bin/env node
+
 var watchr = require('watchr')
 var yaml = require('yaml')
 var fs = require('fs')
 var exec = require('child_process').exec;
 var path = require('path');
+var program = require('commander');
 
 Array.prototype.filterOn = function (field, value) {
 	return this.filter(function(elem) {return elem[field] == value});
@@ -10,17 +13,28 @@ Array.prototype.filterOn = function (field, value) {
 
 // ---
 
+program
+	.version('0.0.1')
+	.option('-o, --outdir <dir>', 'Output directory [out]', String, 'out')
+	.parse(process.argv);
+
+fs.mkdirSync(program.outdir);
 rules = yaml.eval(fs.readFileSync('watchrules.yaml', 'utf8'));
+
+function substituteVars(str, file) {
+	return str
+		.replace('$file', file)
+		.replace('$basename', path.basename(file, path.extname(file)))
+		.replace('$indir', path.dirname(file))
+		.replace('$outdir', program.outdir);
+}
 
 function runExec(array, file) {
 	for (var i = 0; i < array.length; i++) {
-		var ext = path.extname(file);
-		var out = path.dirname(file) + path.sep + path.basename(file, ext);
+		if (file.match(array[i].filter) != null) {
+			console.log(substituteVars(array[i].message, file));
 
-		if (ext.match(array[i].filter) != null) {
-			console.log(array[i].message.replace("$file", file).replace("$out", out));
-
-			exec(array[i].exec.replace("$file", file).replace("$out", out), function(error, stdout, stderr) {
+			exec(substituteVars(array[i].exec, file), function(error, stdout, stderr) {
 				if (error)
 					console.log(stderr);
 			});
@@ -37,7 +51,5 @@ watchr.watch({
 			runExec(rules.filterOn('type', 'dir').filterOn('event', changeType), filePath);
 		else
 			runExec(rules.filterOn('type', 'file').filterOn('event', changeType), filePath);
-
-		// console.log(filePath + ' - ' + changeType);
 	}
 });
